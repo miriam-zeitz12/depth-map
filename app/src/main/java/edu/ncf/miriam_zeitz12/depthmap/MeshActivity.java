366927ab4f258dfaa40bb8cd5221b05bdd687c43
Miriam Zeitz <miriam.zeitz12@ncf.edu>
2015-05-06 19:52:20 -0400

    Added camera functionality and meshactivity

---
+++ app/src/main/java/edu/ncf/miriam_zeitz12/depthmap/MeshActivity.java
@@ -0,0 +1,43 @@
+package edu.ncf.miriam_zeitz12.depthmap;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MeshActivity extends ActionBarActivity {
+
+    private Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        imageUri = Uri.parse(intent.getStringExtra(MainActivity.EXTRA_IMAGE_URI));
+        setContentView(R.layout.activity_mesh);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_mesh, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

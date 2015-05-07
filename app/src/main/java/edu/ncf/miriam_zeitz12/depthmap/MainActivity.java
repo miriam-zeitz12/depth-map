a0fe8d4f400ddd11832284386e14a2b4f8a8f011
Miriam Zeitz <miriam.zeitz12@ncf.edu>
2015-05-07 12:16:37 -0400

    image retrieval from gallery

--- app/src/main/java/edu/ncf/miriam_zeitz12/depthmap/MainActivity.java
+++ app/src/main/java/edu/ncf/miriam_zeitz12/depthmap/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.ncf.miriam_zeitz12.depthmap;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +10,10 @@ import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int ACTIVITY_SELECT_IMAGE = 100;
+
+    public static final String EXTRA_IMAGE_URI = "com.example.miriamzeitz.IMAGE_SELECTED";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +50,34 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void getPhotoFromFiles(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, ACTIVITY_SELECT_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageChosenIntent) {
+        super.onActivityResult(requestCode, resultCode, imageChosenIntent);
+
+        if (requestCode == ACTIVITY_SELECT_IMAGE) {
+            Uri chosenImage = imageChosenIntent.getData();
+            startMeshActivity(chosenImage);
+        }
+
+    }
+
+    public void startMeshActivity(Uri imageUri) {
+
+
+         Intent intent = new Intent(this, MeshActivity.class);
+        intent.putExtra(EXTRA_IMAGE_URI,imageUri.toString());
+
+         startActivity(intent);
 
     }
 
     public void getPhotoFromCamera(View view) {
+
         
     }
 }

4a11093cbc84b45533fb9a05006e982026d01365
sarah-russell12 <sarah.russell12@ncf.edu>
2015-05-07 14:41:52 -0400

    Added the valid email format checking function to the email activity.

--- app/src/main/java/edu/ncf/miriam_zeitz12/depthmap/EmailFileActivity.java
+++ app/src/main/java/edu/ncf/miriam_zeitz12/depthmap/EmailFileActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.apache.http.protocol.HTTP;
 
@@ -42,19 +43,45 @@ public class EmailFileActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private boolean isValidEmail(String email) {
+        // Email must contain an @ to separate the local and domain
+        if (!(email.contains("@"))) return false;
+        // Only one @ can be in an email address
+        if (email.indexOf("@") != email.lastIndexOf("@")) return false;
+        // An email cannot have two consecutive periods (..)
+        for (int i = 0; i < email.length(); i++) {
+            char c = email.charAt(i);
+            if (c == '.') {
+                // Cannot have a leading or trailing period
+                if (i == 0 || i== email.length() - 1) return false;
+                if (email.charAt(i + 1) == '.') return false;
+            }
+        }
+        // This is probably nowhere near as complete a check of a valid email address
+        // Just check out the wiki, the stuff with special characters is very complex
+        // and gives a lot of leniency.  This covers the basics of common email addresses
+        return true;
+    }
+
     public void sendEmail(View view) {
         EditText editText = (EditText) findViewById(R.id.enter_email);
         String email = editText.getText().toString();
+        // Trim leading and trailing whitespace
+        email = email.trim();
         //Code taken from the android studio tutorials
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setType(HTTP.PLAIN_TEXT_TYPE);
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, email); // recipients
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your 3D Printable File");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Hello! \n\n A request has been made to send a "
-        + "3D-printable file to this E-Mail address from an application on a mobile phone.  "
-        + "Attatched is the file requested. Enjoy!");
-        emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://createdFiles/file"));
+        if (isValidEmail(email)) {
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setType(HTTP.PLAIN_TEXT_TYPE);
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, email); // recipients
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your 3D Printable File");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Hello! \n\n A request has been made to send a "
+            + "3D-printable file to this E-Mail address from an application on a mobile phone.  "
+            + "Attached is the file requested. Enjoy!");
+            emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://createdFiles/file"));
 
-        startActivity(emailIntent);
+            startActivity(emailIntent);
+        } else {
+            Toast.makeText(this, "Not a valid email format try again.", Toast.LENGTH_LONG).show();
+        }
     }
 }

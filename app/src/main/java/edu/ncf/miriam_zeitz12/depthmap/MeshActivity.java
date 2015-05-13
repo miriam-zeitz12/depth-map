1d1f01b859624c92dba6858e16d7b3f802c82e6b
Vinushika <stefan.schalk12@ncf.edu>
2015-05-13 10:40:49 -0400

    Now we should in theory be able to access the server from here.

--- app/src/main/java/edu/ncf/miriam_zeitz12/depthmap/MeshActivity.java
+++ app/src/main/java/edu/ncf/miriam_zeitz12/depthmap/MeshActivity.java
@@ -3,6 +3,8 @@ package edu.ncf.miriam_zeitz12.depthmap;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -13,11 +15,24 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -26,6 +41,7 @@ public class MeshActivity extends ActionBarActivity {
     private Uri imageUri;
     private static final String baseObjName = "_3d_model.obj";
     public static final String EMAIL_IMAGE_URI = "com.example.miriamzeitz.IMAGE_PROCESSED";
+    public static final String REQUEST_PATH = "162.243.209.132:9999/create";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +49,7 @@ public class MeshActivity extends ActionBarActivity {
         Intent intent = getIntent();
         imageUri = Uri.parse(intent.getStringExtra(MainActivity.EXTRA_IMAGE_URI));
         setContentView(R.layout.activity_mesh);
+        Context context = this.getApplicationContext();
         try {
             //first check if we can write the OBJ
             if (isExternalStorageWritable()) {
@@ -40,14 +57,54 @@ public class MeshActivity extends ActionBarActivity {
                 String fullFileName  = UUID.randomUUID().toString() + baseObjName;
                 File outFile = new File(Environment.getExternalStoragePublicDirectory(
                         Environment.DIRECTORY_DOCUMENTS), fullFileName);
-                String outPath = outFile.getAbsolutePath();
-                MeshCreator creator = new MeshCreator(imageUri,this);
-                List<Point3D> pointCloud = creator.getPoints();
-                PhotoObjWriter writer = new PhotoObjWriter(this,outPath);
-                writer.writePhotoObj(creator.getWidth(), creator.getHeight(), pointCloud);
-                //done, now email
+                String fullPath = outFile.getAbsolutePath();
+                ContentResolver resolver = context.getContentResolver();
+                DataExtractor extractor = new DataExtractor((resolver.openInputStream(imageUri)));
+                byte[] data = extractor.getDepthData();
+                if (data == null) {
+                    displayBadFileDialog();
+                }
+                double near = extractor.getNear();
+                double far = extractor.getFar();
+                Log.d("Found data",Double.toString(near)+" "+Double.toString(far));
+                Log.d("Length of data:",Integer.toString(data.length));
+                Log.d("First byte in data:",Byte.toString(data[0]));
+                Log.d("Last byte in data:",Byte.toString(data[data.length-1]));
+                //create HttpClient
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+
+                //make a POST request object to our path
+                HttpPost httpPost = new HttpPost(REQUEST_PATH);
+
+                //make a Map for JSON to work
+                Map<String, String> jsonSource = new HashMap<>();
+
+                JSONObject jsonHolder = new JSONObject();
+                jsonHolder.put("near",near);
+                jsonHolder.put("far",far);
+                jsonHolder.put("imageData",new String(data));
+                Log.d("Starting POST","JSON Data is ready.");
+
+                //pass this to a StringEntity, because abstractions
+                StringEntity stringEntity = new StringEntity(jsonHolder.toString());
+
+                httpPost.setEntity(stringEntity);
+
+                //make sure we get the right headers so the server knows what to do with it
+                httpPost.setHeader("Accept","application/json");
+                httpPost.setHeader("Content-type","application/json");
+
+                //and now execute it
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                String response = httpClient.execute(httpPost,responseHandler);
+                //this is the header so now let's write to the file...
+                PrintWriter outputStream = new PrintWriter(new FileOutputStream(outFile));
+                outputStream.write(response);
+                outputStream.close();
+
+                //and now email this thing
                 Intent emailIntent = new Intent(this,EmailFileActivity.class);
-                emailIntent.putExtra(EMAIL_IMAGE_URI,fullFileName);
+                emailIntent.putExtra(EMAIL_IMAGE_URI,fullPath);
                 startActivity(emailIntent);
             } else {
                 displayNoAccessDialog();
@@ -56,6 +113,9 @@ public class MeshActivity extends ActionBarActivity {
             //if we can't, then display this error message
             Log.wtf("arf", "no file?", e);
             displayBadFileDialog();
+        } catch (JSONException f) {
+            Log.wtf("No JSON?","JSONException thrown",f);
+            displayBadJSONDialog();
         }
     }
     /**
@@ -67,6 +127,24 @@ public class MeshActivity extends ActionBarActivity {
         new AlertDialog.Builder(this)
                 .setTitle("File not found")
                 .setMessage("The image file you wished to process could not be found." +
+                        " Please try to take your picture again")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(us, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }).setIcon(android.R.drawable.ic_dialog_alert).show();
+    }
+
+    /**
+     * Displays an alert dialog for when JSON fails.
+     */
+    private void displayBadJSONDialog() {
+        final Activity us = this;
+        new AlertDialog.Builder(this)
+                .setTitle("File not found")
+                .setMessage("The image file you wished to process could not be converted to Base64." +
                         " Please try to take your picture again")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     @Override
